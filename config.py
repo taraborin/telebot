b3d8d84e57e3124a93c4b28aec393a50016c7d88
taraborin <99545634+taraborin@users.noreply.github.com>
2022-02-13 17:53:28 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 img_size_cpu = 128  
 img_size_gpu = 512
-token = r"5209760713:AAGD4Bes-hB8OYWWX6Ni75KmhQsnWZGmHLg"
+token = r"5209760713:AAFgjV-MpySVvg7IxeartArY5TK808nIDts"
 file_cache_nn = 'vgg19_cache.pth'
   
